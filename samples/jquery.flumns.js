303de380d273dd9b5a9053d134031b31d473c273
rexblack <mail@benignware.com>
2014-01-21 23:00:01 +0100

    validation cache refactoring

--- samples/jquery.flumns.js
+++ samples/jquery.flumns.js
@@ -1,24 +1,23 @@
 +function ( $, window) {
 
   var pluginName = 'flumns';
-  
+
   var defaults = {
     align: 'justify',
-    columnWidth: 292.5, 
-    grid: true, 
-    columnPrefix: "", 
-    columnOffsetPrefix: "", 
-    bindResize: true, 
-    masonry: false, 
-    resizeDelay: 100, 
+    columnWidth: 292.5,
+    grid: true,
+    columnPrefix: "",
+    columnOffsetPrefix: "",
+    bindResize: true,
+    masonry: false,
     // callbacks
-    beforeLayout: null, 
-    layout: null, 
-    render: null, 
-    itemsChanged: null
+    beforeLayout: null,
+    layout: null,
+    render: null,
+    itemsAdded: null
   };
-  
-  
+
+
   (function() {
     var lastTime = 0;
     var vendors = ['webkit', 'moz', 'o', 'ms'];
@@ -45,18 +44,8 @@
   }());
 
 
-  function setCSS(element, properties) {
-    var props = [];
-    for (var name in properties) {
-      var value = properties[name];
-      if (value) {
-        props.push(name + ": " + value);
-      }
-    }
-    var cssText = props.join("; ");
-    $(element).css('cssText', cssText);
-  }
-  
+
+
   function getClientRect(element) {
     // ie8 compatible
     var rect = element.getBoundingClientRect();
@@ -68,61 +57,51 @@
     result.height = typeof(rect.height) == "number" ? rect.height : rect.bottom - rect.top;
     return result;
   }
-  
+
   function Flumns(element, options) {
-    
+
     var instance = this;
-    
+
     var invalidateFlag = true;
-    
+
     var $element = $(element);
     var columns = [];
     var items = [];
-    var itemsChanged = false;
-    
-    // cache validation properties
-    
-    var _itemsValid = true;
-    
-    var _width = 0;
-    var _height = 0;
-    
+    var itemsAdded = false;
+
+    // column count
     var _columnCount = 0;
     var _columnWidth = 0;
-    var _columnWeight = 0;
-    var _columnOffset = 0;
-    
-    var resizeTimeoutId = null;
-    
+
     // debug
     var execTime = 0;
-    
+
     function isItem(elem) {
       return elem.nodeType == 1
         && $.inArray(elem.localName.toLowerCase(), ["br", "script", "link", "map"]) == -1
         || elem.nodeType == 3 && $.trim(elem.nodeValue);
     }
-    
+
     this.indexOf = function(item) {
       for (var i = 0; i < items.length; i++) if (items[i] == item) return i;
     };
-    
+
     this.get = function(index) {
       return items[index];
     };
-    
+
     this.size = function() {
       return items.length;
     };
-    
+
     this.add = function(item, index) {
       if (isItem(item)) {
         items.splice(index, 0, item);
-        _itemsValid = false;
+        itemsAdded = true;
         this.invalidate();
       }
     };
-    
+
     // TODO: use document fragment for add all
     this.addAll = function(collection, index) {
       var invalidate = invalidateFlag;
@@ -136,14 +115,13 @@
         this.invalidate();
       }
     };
-    
+
     this.remove = function(item) {
       items.splice(this.indexOf(item), 1);
-      $(item).remove();
-      _itemsValid = false;
+      $(item).detach();
       this.invalidate();
     };
-    
+
     this.removeAll = function() {
       var invalidate = invalidateFlag;
       for (var i = 0; i < this.size(); i++) {
@@ -156,7 +134,7 @@
         this.invalidate();
       }
     };
-    
+
     this.replaceAll = function(items) {
       var invalidate = invalidateFlag;
       invalidateFlag = false;
@@ -167,13 +145,19 @@
         this.invalidate();
       }
     };
-    
+
+    this.invalidate = function() {
+      if (!invalidateFlag) return;
+      _columnCount = 0;
+      layout.call(this);
+    };
+
     this.destroy = function() {
       $(window).unbind('resize', resizeHandler);
     };
-    
+
     // TODO: has option changed (arrays)
-    
+
     this.setOptions = function(obj) {
       var changed = false;
       invalidateFlag = false;
@@ -189,11 +173,11 @@
         this.invalidate();
       }
     };
-    
+
     this.getOptions = function() {
       return options;
     };
-    
+
     this.setOption = function(name, value) {
       var invalidate = invalidateFlag;
       if (options[name] != value) {
@@ -202,7 +186,7 @@
         options[name] = value;
         // option specific behavior / invalidation
         switch (name) {
-          case 'items': 
+          case 'items':
             this.replaceAll(value);
             break;
         }
@@ -212,84 +196,151 @@
         }
       }
     };
-    
+
     this.getOption = function(name) {
       return options[name];
     };
-    
+
     this.getColumnCount = function() {
       return _columnCount;
     };
-    
+
     this.getColumnWidth = function() {
       return _columnWidth;
     };
-    
-    this.getColumnOffset = function() {
-      return _columnOffset;
-    };
-    
-    this.getColumnWeight = function() {
-      return _columnWeight;
-    };
-    
+
     this.getExecTime = function() {
       return execTime;
     };
-    
-    this.invalidate = function() {
-      if (!invalidateFlag) return;
-      _width = 0;
-      _height = 0;
-      _columnCount = 0;
-      console.log("INVALIDATE", _columnCount);
-      layout.call(this);
-    };
-    
+
     function getGridClass(prefixOption, columnWeight) {
       var classes = [], columnPrefixes = typeof prefixOption == 'string' ? [prefixOption] : prefixOption;
       for (var i = 0; i < columnPrefixes.length; i++) classes.push(columnPrefixes[i] + columnWeight);
       return classes.join(" ");
     }
-    
+
     function getColumnWidthOption() {
       var columnWidthOption = options.columnWidth;
       switch (typeof columnWidthOption) {
-        case 'number': 
+        case 'number':
           return columnWidthOption;
-        case 'string': 
+        case 'string':
           if (!isNaN(parseFloat(columnWidthOption))) {
             return parseFloat(columnWidthOption);
           }
-        default: 
+        default:
           // auto
           return defaults.columnWidth;
        }
     }
-    
+
     function getGridOption() {
       var gridOption = options.grid;
       switch (typeof gridOption) {
-        case 'number': 
+        case 'number':
           return parseInt(gridOption);
-        case 'boolean': 
+        case 'boolean':
           return gridOption ? Flumns.GRID_BASE : 0;
-        case 'string': 
+        case 'string':
           return gridOption == "true" ? Flumns.GRID_BASE : 0;
-        case 'undefined': 
+        case 'undefined':
           return 0;
-        default: 
+        default:
           return Flumns.GRID_BASE;
       }
     }
-    
-    function layoutAlign() {
-      // align columns
-      var width = _width;
+
+
+
+    // heart of the plugin
+    function layout() {
+
+
+
+
+      // console.time("layout");
+
+      // layout
+
+      // var h = getClientRect(element).height;
+      // $element.css('height', h + "px");
+      // console.log("height: ", h, $element.height(), element);
+
+      var columnWidthOption = getColumnWidthOption();
+      var gridOption = getGridOption();
       var align = options.align;
-      
-      console.log("layout align: ", width, align);
-      
+
+      var width = getClientRect(element).width;
+
+      var columnFloat = width / columnWidthOption;
+      var columnCount = Math.max(columnFloat | 0,1);
+
+      var columnOffset = columnFloat - columnCount;
+
+      var columnWeightFloat = (gridOption || Flumns.GRID_BASE) / columnCount;
+      var columnWeight = columnWeightFloat | 0;
+
+      var columnWidth = columnWidthOption;
+
+      if (align == "justify") {
+        if (gridOption) {
+          // adjust to grid column weight
+          columnCount = gridOption / columnWeight;
+        }
+        columnWidth = width / columnCount;
+      } else if (gridOption) {
+        // adjust grid column weight
+        columnWeight = gridOption / columnCount | 0;
+        columnWidth = width / gridOption * columnWeight;
+      }
+
+      // callback
+      if (typeof options.beforeLayout  == 'function') {
+        options.beforeLayout.call(this);
+      }
+
+      if (columnCount != _columnCount) {
+
+        _columnCount = columnCount;
+        _columnWidth = columnWidth;
+
+        // setup columns
+
+        for (var c = 0; c < columnCount; c++) {
+
+          var column = columns[c] || {css: {}, className: '', height: 0};
+          column.element = column.element || (function() {
+            return $(document.createElement('div'));
+          })();
+
+          var css = column.css;
+
+          if (!gridOption || !options.columnPrefix) {
+            css.width = align == "justify" ? 100 / columnCount + "%" : columnWidth + "px";
+          } else {
+            css.width = '';
+          }
+
+          if (options.columnPrefix) {
+            css.display = '';
+            css.verticalAlign = '';
+            column.className = getGridClass(options.columnPrefix, columnWeight);
+          } else {
+            css.display = 'inline-block';
+            css.verticalAlign = 'top';
+          }
+          column.css = css;
+
+
+          columns[c] = column;
+
+        }
+
+      }
+
+
+
+      // align columns
       var alignFloat = align == "center" ? 0.5 : align == "right" ? 1 : 0;
       var totalColumnWidth = this.getColumnWidth() * this.getColumnCount();
       for (var i = 0; i < columns.length; i++) {
@@ -304,157 +355,22 @@
           css.left = '';
         }
       }
-    }
-   
-    function validateSize() {
-      var valid = _width && _height ? true : false;
-      var clientRect = getClientRect(element);
-      if (valid && (clientRect.width != _width || clientRect.height != _height)) {
-        valid = false;
-      }
-      _width = clientRect.width;
-      _height = clientRect.height;
-      return valid;
-    }
-    
-    function validateColumns() {
-      
-      var valid = true;
-      
-      var columnWidth = getColumnWidthOption();
-      var gridOption = getGridOption();
-      var align = options.align;
-      
-      var width = _width;
-      
-      var columnFloat = width / columnWidth;
-      var columnCount = Math.max(columnFloat | 0, 1);
-      var columnOffset = columnFloat - columnCount;
-      
-      var columnWeightFloat = (gridOption || Flumns.GRID_BASE) / columnCount;
-      var columnWeight = columnWeightFloat | 0;
-      
-      if (align == "justify") {
-        if (gridOption) {
-          // adjust to grid column weight
-          columnCount = gridOption / columnWeight;
-        }
-        columnWidth = width / columnCount;
-      } else if (gridOption) {
-        // adjust grid column weight
-        columnWeight = gridOption / columnCount | 0;
-        columnWidth = width / gridOption * columnWeight;
-      }
-      
-      if (columnCount != _columnCount || columns.length < _columnCount) {
-        valid = false;
-      }
-      
-      _columnCount = columnCount; 
-      _columnWidth = columnWidth;
-      _columnWeight = columnWeight;
-      _columnOffset = columnOffset;
-      
-      return valid;
-    
-    }
-    
-    function validateItems() {
-      var result = _itemsValid;
-      _itemsValid = true;
-      return result;
-    } 
-    
-    function layout() {
 
-      console.time('layout');
-      
-      if (typeof options.beforeLayout  == 'function') {
-        options.beforeLayout.call(this);
-      }
-      
-      var sizeValid = validateSize.call(this);
-      var columnsValid = validateColumns.call(this);
-      var itemsValid = validateItems.call(this);
-      
-      console.log("LAYOUT", _columnCount, _columnWidth, _columnWeight, _columnOffset);
-      console.log("sizeValid: ", sizeValid);
-      console.log("columnsValid: ", columnsValid);
-      console.log("itemsValid: ", itemsValid);
-      
-      if (!columnsValid) {
-        layoutColumns.call(this);
-      }
-      
-      if (!columnsValid || !sizeValid) {
-        layoutAlign.call(this);
-        renderColumns.call(this);
-      }
-      
-      if (!columnsValid || !itemsValid) {
+      var instance = this;
 
-        layoutItems.call(this);
-      
-        if (options.masonry && this.getColumnCount() > 1) {
-          renderItems.call(this);
-          layoutMasonry.call(this);
-        }
+      window.requestAnimationFrame(function() {
+        // render
+        renderColumns.call(instance);
+      });
 
-        renderItems.call(this);
-      }
-      
+      // console.timeEnd("layout");
+
       // callback
       if (typeof options.layout  == 'function') {
         options.layout.call(this);
       }
-      
-      console.timeEnd('layout');
     }
-    
-    
-    function layoutColumns() {
 
-      // callback
-      if (typeof options.beforeLayout  == 'function') {
-        options.beforeLayout.call(this);
-      }
-      
-      var gridOption = getGridOption();
-      var align = options.align;
-        
-      var columnCount = _columnCount;
-      var columnWidth = _columnWidth;
-      var columnWeight = _columnWeight;
-      var columnOffset = _columnOffset;
-      
-      for (var c = 0; c < columnCount; c++) {
-        var column = columns[c] || {css: {}, className: '', height: 0};
-        column.element = column.element || (function() {
-          return $(document.createElement('div')); 
-        })();
-
-        var css = column.css;
-          
-        if (!gridOption || !options.columnPrefix) {
-          css.width = align == "justify" ? 100 / columnCount + "%" : columnWidth + "px";
-        } else {
-          css.width = '';
-        }
-        
-        if (options.columnPrefix) {
-          css.display = '';
-          css.verticalAlign = '';
-          column.className = getGridClass(options.columnPrefix, columnWeight);
-        } else {
-          css.display = 'inline-block';
-          css.verticalAlign = 'top';
-        }
-        column.css = css;
-        columns[c] = column;
-      }
-      
-    }
-    
     function layoutItems() {
       var columnCount = instance.getColumnCount();
       for (var i = 0; i < items.length; i++) {
@@ -466,97 +382,127 @@
         column.fragment.appendChild(item);
       }
     }
-    
+
     function layoutMasonry() {
-      var columnCount = instance.getColumnCount(); 
-      var columnHeights = [];
-      var itemHeights = [];
-      for (var i = 0; i < items.length; i++) {
-        itemHeights[i] = getClientRect(items[i]).height;
+
+      var columnCount = instance.getColumnCount();
+
+      // reset column heights
+      for (var c = 0; c < columns.length; c++) {
+        columns[c].height = 0;
       }
+
       for (var i = 0; i < items.length; i++) {
+
          var item = items[i];
          var columnIndex = 0;
+
          if (i >= columnCount) {
            var minHeight = 0;
            for (var c = 0; c < columnCount; c++) {
              var column = columns[c];
-             if (minHeight == 0 || columnHeights[c] < minHeight) {
-               minHeight = columnHeights[c];
+             if (minHeight == 0 || column.height < minHeight) {
+               minHeight = column.height;
                columnIndex = c;
              }
            }
          } else {
            columnIndex = i % columnCount;
          }
+
          var column = columns[columnIndex];
-         columnHeights[columnIndex] = columnHeights[columnIndex] || 0;
-         columnHeights[columnIndex]+= itemHeights[i];
+         column.height+= getClientRect(item).height;
+
          column.fragment = column.fragment || document.createDocumentFragment();
          column.fragment.appendChild(item);
+
       }
     }
-    
+
     function renderColumns() {
+
+      // console.time("render");
+
+      layoutItems();
+
       var columnCount = instance.getColumnCount();
       var len = Math.max(columnCount, columns.length);
       for (var c = 0; c < len; c++) {
+
         var column = columns[c];
         var columnElement = column.element;
+
         if (c > $element.children().length - 1) {
           $element.append(column.element);
         }
+
         if (c < columnCount) {
+
           columnElement.removeClass();
           columnElement.addClass(column.className);
-          setCSS(columnElement, column.css);
+          columnElement.css(column.css);
+
         } else {
           columnElement.remove();
         }
+
+      }
+
+      renderItems();
+
+      if (options.masonry && columnCount > 1) {
+        layoutMasonry();
+        renderItems();
+      }
+
+      // console.timeEnd("render");
+
+      // callback
+      if (itemsAdded) {
+        if (typeof options.itemsAdded  == 'function') {
+          options.itemsAdded.call(this);
+        }
+        itemsAdded = false;
+      }
+
+      if (typeof options.render  == 'function') {
+        options.render.call(this);
       }
     }
-    
+
     function renderItems() {
-      // keyboard is natively dismissed when removed from dom
-      // $element.find(':focus').blur();
+
       var columnCount = instance.getColumnCount();
       for (var c = 0; c < columnCount; c++) {
         var column = columns[c];
         if (column.fragment) {
           column.element.append(column.fragment);
           column.fragment = null;
+
         }
       }
     }
-  
-    function triggerLayout() {
-      window.requestAnimationFrame(function() {
-        layout.call(instance);
-      });
-    }
-   
+
     function resizeHandler(event) {
-      window.clearTimeout(resizeTimeoutId);
-      if (options.resizeDelay > 0) {
-        resizeTimeoutId = window.setTimeout(function() {
-          triggerLayout();
-        }, options.resizeDelay);
+      focusedElement = $element.find(':focus')
+      if (focusedElement.length === 0) {
+        layout.call(instance);
       } else {
-        triggerLayout();
+        focusedElement.one('blur', resizeHandler)
       }
     }
-    
+
     function init() {
       if (options.bindResize) {
         $(window).bind('resize', resizeHandler);
       }
       this.addAll($element.children());
     }
-    
+
     init.call(this);
-    
+
   };
-  
+
   Flumns.GRID_BASE = 12;
 
   // bootstrap plugin
